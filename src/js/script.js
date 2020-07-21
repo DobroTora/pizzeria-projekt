5d6278f465583f51f1930c867fc91e1af4e84566
Debra <48626342+DobroTora@users.noreply.github.com>
2020-07-21 19:45:34 +0100

    testing objects for products data

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,27 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(){
+      const thisProduct = this;
+      console.log('new Product:', thisProduct);
+    }
+  }
+
   const app = {
+    
+    initMenu: function(){
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+      const testProduct = new Product();
+      console.log('testProduct:', testProduct);
+    },
+
+    initData: function(){
+      const thisApp = this;
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,8 +80,12 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
+
+
 
   app.init();
 }
