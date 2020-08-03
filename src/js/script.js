b6a5ad4574b0eaf1ab07f36ef7f171dc0e8093ee
Debra <48626342+DobroTora@users.noreply.github.com>
2020-08-03 23:55:47 +0100

    initActions on Cart

--- src/js/script.js
+++ src/js/script.js
@@ -256,9 +256,31 @@ const templates = {
       // console.log('AmountWidget', thisWidget);
       // console.log('constructor arguments', element);
     }
+
+    class Cart {
+      constructor(element) {
+        const thisCart = this;
+        thisCart.products = [];
+        thisCart.getElements(element);
+        console.log('new Cart', thisCart);
+      }
+
+      getElements(element) {
+        const thisCart = this;
+        thisCart.dom = {};
+        thisCart.dom.wrapper = element;
+        thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+
+      }
+
+      initActions() {
+        const thisCart = this;
+  
+        thisCart.dom.toggleTrigger.addEventListener('click', function () {
+          thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+        });
+      }
   }
-
-
 
 
   const app = {
@@ -276,6 +298,12 @@ const templates = {
       thisApp.data = dataSource;
     },
 
+    initCart: function () {
+      const thisApp = this;
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -285,6 +313,7 @@ const templates = {
       console.log('templates:', templates);
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
