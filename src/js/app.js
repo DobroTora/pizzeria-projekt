5cbfec7d96f29ec2d407c5b318e70efc1dffe838
Debra <48626342+DobroTora@users.noreply.github.com>
2020-08-22 00:36:14 +0100

    created initPages

--- src/js/app.js
+++ src/js/app.js
@@ -14,24 +14,44 @@ const app = {
       }
     },
     
-    initPages: function(){
+    activatePage: function(pageId){
+    const thisApp = this;
+    for (let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    for (let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
+  initPages: function(){
     const thisApp = this;
 
-    thisApp.pages = Array.from(document.querySelector(select.containerOf.pages).children);
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
     console.log(thisApp.pages);
     thisApp.navLinks = document.querySelectorAll(select.nav.links);
-    console.log(thisApp.navLinks)
-    thisApp.activatePage(thisApp.pages[0].id);
-    for (let link of thisApp.navLinks) {
-        link.getEventListener('click', function(event){
-            const clickedElement = this;
-            event.preventDefault();
-            const id = clickedElem.getAttribute('href').replace('#', '');
-            thisApp.activatePage(id);
-            window.location.hash = '#/' + id;
-        })
+    const idFromHash = window.location.hash.replace('#/', '');
+    let pageMatchingHash = thisApp.pages[0].id;
+    for (let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
     }
-        
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function(event){
+        const clickedElem = this;
+        event.preventDefault();
+        const id = clickedElem.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+        window.location.hash = '#/' + id;
+      });
+    }
   },
 
     initData: function(){
