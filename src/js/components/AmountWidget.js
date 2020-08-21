6276a6194377314e801cb3bbf019c78b16ec4fd9
Debra <48626342+DobroTora@users.noreply.github.com>
2020-08-21 19:18:30 +0100

    update import

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,4 +1,6 @@
- export class AmountWidget {
+import {select, settings} from '../settings.js';
+
+export class AmountWidget {
     constructor(element) {
       const thisWidget = this;
       thisWidget.getElements(element);
